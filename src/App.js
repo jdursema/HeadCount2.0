e9c30bb2893ae5c0c7fd00a9dfcc93e95535e1f0
Julie Dursema <jmdursema@gmail.com>
2017-11-30 15:56:23 -0700

    create compare data card and render it

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import CardContainer from './CardContainer';
 import Search from './Search';
 import CompareContainer from './CompareContainer'
 
+const district = new DistrictRepository(kinderData);
+
 
 class App extends Component {
   constructor() {
@@ -18,21 +20,20 @@ class App extends Component {
 
     this.changeClass = this.changeClass.bind(this);
     this.search = this.search.bind(this);
+    this.compareCard = this.compareCard.bind(this);
   }
+  
 
 componentDidMount() {
-  const district = new DistrictRepository(kinderData)
   this.setState({data: district.data})
 }
 
 search(string)  {
-  const district = new DistrictRepository(kinderData)
   let filteredData= district.findAllMatches(string)
   this.setState({data: filteredData})
 }
 
 changeClass(location) {
-  const district = new DistrictRepository(kinderData);
   let filteredArray = this.state.selectedArray.filter(dataPiece => {
     return dataPiece.location === location
   })
@@ -53,16 +54,15 @@ changeClass(location) {
   
 }
 
-compareCards(location1, location2) {
-  const district = new DistrictRepository(kinderData)
-
+compareCard(location1, location2) {
+  return district.compareDistrictAverages(location1, location2)
 }
 
   render() {
     return (
       <div>
         <Header />
-        <CompareContainer selectedArray={this.state.selectedArray}changeClass={this.changeClass}/>
+        <CompareContainer selectedArray={this.state.selectedArray} changeClass={this.changeClass} compareCard = {this.compareCard}/>
         <Search search={this.search}/>
         <CardContainer schoolData={this.state.data} changeClass={this.changeClass} selectedCards = {this.state.selectedArray}/> 
       </div>
--- src/CompareContainer.js
+++ src/CompareContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from './Card';
 
-const CompareContainer = ({selectedArray, changeClass}) => {
+const CompareContainer = ({selectedArray, changeClass, compareCard}) => {
   const mappedSectedCards= selectedArray.map((selectedCard)=>{
     return <Card key={selectedCard.location} 
                      location={selectedCard.location} 
@@ -9,11 +9,39 @@ const CompareContainer = ({selectedArray, changeClass}) => {
                      changeClass={changeClass}
                      type = 'card'
                      />
-  })
+  });
+  let comparisonData;
+  let keys;
+  let mappedCompared;
+
+  if (mappedSectedCards.length === 2) {
+    comparisonData= compareCard(selectedArray[0].location, selectedArray[1].location) 
+    keys= Object.keys(comparisonData)
+    mappedCompared= keys.map((key)=>{
+      return(
+        <div>
+          <h3>{key}</h3>
+          <h4>{comparisonData[key]}</h4>
+          <h5>{comparisonData.compared}</h5>
+        </div> 
+      )
+    })
+  }
+
+
+  
+  
   return(
     <div>
-
-      {mappedSectedCards}
+      {
+        mappedSectedCards[0] &&
+        mappedSectedCards[0]
+      }
+      {mappedCompared}
+      {
+        mappedSectedCards[1] &&
+        mappedSectedCards[1]
+      }
     </div>
   )
 }
